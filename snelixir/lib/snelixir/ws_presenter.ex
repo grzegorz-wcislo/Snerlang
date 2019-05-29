1f86706e00d3f29c06e6601d1f3e4560dd0f570d
Grzegorz Wcisło <grzegorz.oskar.wcislo@gmail.com>
2019-05-29 11:08:23 +0200

    Add sensible snake id generation

--- snelixir/lib/snelixir/ws_presenter.ex
+++ snelixir/lib/snelixir/ws_presenter.ex
@@ -2,18 +2,26 @@ defmodule Snelixir.WsPresenter do
   def lobby_message(snake, count) do
     JSON.encode!(%{
       type: :lobby,
-      id: snake,
+      id: pid_to_id(snake),
       count: count,
       fullCount: Snelixir.GameLogic.max_snake_count()
     })
   end
 
   def game_init_msg({snakes}) do
-    JSON.encode!(%{type: :init_game, snakes: snakes, boardSize: Snelixir.GameLogic.length()})
+    JSON.encode!(%{
+      type: :init_game,
+      snakes: snake_map_view(snakes),
+      boardSize: Snelixir.GameLogic.length()
+    })
   end
 
   def board_msg({snakes, _, apples}) do
-    JSON.encode!(%{type: :board, snakes: snakes, apples: apples})
+    JSON.encode!(%{
+      type: :board,
+      snakes: snake_map_view(snakes),
+      apples: apples
+    })
   end
 
   def win_msg do
@@ -22,5 +30,17 @@ defmodule Snelixir.WsPresenter do
 
   def lose_msg do
     JSON.encode!(%{type: :defeat, header: "Defeat Rustique", msg: nil})
+  end
+
+  defp snake_map_view(snakes) do
+    for {snake_pid, value} <- snakes, into: %{} do
+      {pid_to_id(snake_pid), value}
+    end
+  end
+
+  defp pid_to_id(pid) do
+    :crypto.hash(:md5, inspect(pid))
+    |> Base.encode64()
+    |> String.slice(0..7)
   end
 end
